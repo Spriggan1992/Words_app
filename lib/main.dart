ba14959976a5f2a570008c9a7aa7134f564e3467
Nikita Kozlov <nikita@mac-mini.local>
2020-06-23 20:07:53 +0800

    Correct grammar

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: 'Word App',
         debugShowCheckedModeBanner: false,
         initialRoute: WelcomScreen.id,
         routes: {
