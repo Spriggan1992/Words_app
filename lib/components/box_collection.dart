1d9c010fff10ff6334199ca4d3d556460dbed55e
Roman <spriggan1992@gmail.com>
2020-06-27 02:12:39 +0800

    changed design of BoxCollection

--- lib/components/box_collection.dart
+++ lib/components/box_collection.dart
@@ -36,10 +36,10 @@ class _BoxCollectionState extends State<BoxCollection> {
                   color: Color(0xFF878686),
                   blurRadius: 3.0,
                   spreadRadius: 1.0,
-                  offset: Offset(1, 0.5))
+                  offset: Offset(1, 3))
             ],
             borderRadius: BorderRadius.circular(10.0),
-            color: Colors.white,
+            color: Colors.grey[200],
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -65,6 +65,7 @@ class _BoxCollectionState extends State<BoxCollection> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
+//Show Text.
                       widget.isChecked
                           ? Expanded(
                               child: FittedBox(
@@ -79,6 +80,7 @@ class _BoxCollectionState extends State<BoxCollection> {
                                 ),
                               ),
                             )
+//Show Text field(Chacnge collections name)
                           : Expanded(
                               child: Container(
                                 width: 60,
@@ -103,21 +105,24 @@ class _BoxCollectionState extends State<BoxCollection> {
                                 ),
                               ),
                             ),
+// Indicator amount of words in collection
                       Padding(
                         padding: EdgeInsets.only(left: 5),
                         child: Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.bottomCenter,
-                              end: Alignment.topCenter,
-                              stops: [0.4, 0.5],
-                              colors: [Colors.green, Colors.red[400]],
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                stops: [0.3, 0.7],
+                                colors: [Colors.green, Colors.red[400]],
+                              ),
+                              borderRadius: BorderRadius.circular(20),
                             ),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          height: 60,
-                          width: 30,
-                        ),
+                            height: 60,
+                            width: 30,
+                            child: Center(
+                                child:
+                                    Text('5', style: TextStyle(fontSize: 17)))),
                       ),
                     ],
                   ),
@@ -128,90 +133,3 @@ class _BoxCollectionState extends State<BoxCollection> {
     );
   }
 }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: widget.onTap,
-//       onLongPress: widget.deleteColection,
-//       child: Container(
-//           decoration: BoxDecoration(
-//             boxShadow: [
-//               BoxShadow(
-//                   color: Color(0xFF878686),
-//                   blurRadius: 3.0,
-//                   spreadRadius: 1.0,
-//                   offset: Offset(1, 0.5))
-//             ],
-//             borderRadius: BorderRadius.circular(10.0),
-//             color: Colors.white,
-//           ),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.stretch,
-//             children: <Widget>[
-//               Container(
-//                 padding: EdgeInsets.only(left: 5.0, top: 5),
-//                 child: Text('Eng', style: TextStyle(fontSize: 10.0)),
-//               ),
-//               Container(
-//                 alignment: Alignment.center,
-//                 height: 30.0,
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.only(
-//                     topLeft: Radius.circular(10.0),
-//                     topRight: Radius.circular(10.0),
-//                   ),
-//                 ),
-//                 child: Padding(
-//                   padding: const EdgeInsets.symmetric(horizontal: 20),
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: <Widget>[
-//                       widget.isChecked
-//                           ? FittedBox(
-//                               child: Padding(
-//                                 padding: const EdgeInsets.all(8.0),
-//                                 child: Text(
-//                                   widget.textTitle,
-//                                   style: TextStyle(
-//                                       fontSize: 35.0, color: Colors.black),
-//                                 ),
-//                               ),
-//                             )
-//                           : TextField(
-//                               decoration: InputDecoration(
-//                                   contentPadding: EdgeInsets.only(
-//                                       top: 0,
-//                                       left: 20.0,
-//                                       bottom: 8.0,
-//                                       right: 20.0)),
-//                               cursorColor: Colors.white,
-//                               textAlign: TextAlign.center,
-//                               style: TextStyle(
-//                                 color: Colors.black,
-//                                 fontSize: 35.0,
-//                               ),
-//                               autofocus: true,
-//                               onSubmitted: widget.onSubmite,
-//                               controller: TextEditingController(
-//                                 text: widget.textTitle,
-//                               ),
-//                             ),
-//                       // Container(
-//                       //   padding: EdgeInsets.only(right: 20),
-//                       //   decoration: BoxDecoration(
-//                       //     color: Colors.green,
-//                       //     borderRadius: BorderRadius.circular(20),
-//                       //   ),
-//                       //   height: 60,
-//                       //   width: 30,
-//                       // ),
-//                     ],
-//                   ),
-//                 ),
-//               )
-//             ],
-//           )),
-//     );
-//   }
-// }
