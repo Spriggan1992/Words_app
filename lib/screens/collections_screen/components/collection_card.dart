48ad2fce9d3077163095970ab0b6c0d46a3be27d
Nikita Kozlov <nikita@mac-mini.local>
2020-08-25 16:15:40 +0300

    refactor animation to separeta file

--- lib/screens/collections_screen/components/collection_card.dart
+++ lib/screens/collections_screen/components/collection_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:words_app/animations/shake_animation.dart';
 import 'package:words_app/bloc/collections/collections_bloc.dart';
 import 'package:words_app/helpers/functions.dart';
 
@@ -10,7 +11,7 @@ import 'package:words_app/components/my_separator.dart';
 import 'btns.dart';
 import 'text_holder.dart';
 
-class CollectionCard extends StatefulWidget {
+class CollectionCard extends StatelessWidget {
   CollectionCard({
     this.goToManagerCollections,
     this.index,
@@ -24,74 +25,10 @@ class CollectionCard extends StatefulWidget {
   final Function showEditDialog;
 
   @override
-  _CollectionCardState createState() => _CollectionCardState();
-}
-
-class _CollectionCardState extends State<CollectionCard>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation rotateAnimation;
-
-  static final tweenSequence = TweenSequence(<TweenSequenceItem<double>>[
-    TweenSequenceItem<double>(
-        tween: Tween<double>(begin: 0.0, end: 0.2)
-            .chain(CurveTween(curve: Curves.easeIn)),
-        weight: 2),
-    TweenSequenceItem<double>(
-        tween: Tween<double>(begin: 0.0, end: -0.2)
-            .chain(CurveTween(curve: Curves.easeInOut)),
-        weight: 2),
-    TweenSequenceItem<double>(
-        tween: Tween<double>(begin: 0.0, end: 0.2)
-            .chain(CurveTween(curve: Curves.easeIn)),
-        weight: 2),
-    TweenSequenceItem<double>(
-        tween: Tween<double>(begin: 0.0, end: -0.2)
-            .chain(CurveTween(curve: Curves.easeInOut)),
-        weight: 2),
-    TweenSequenceItem<double>(
-        tween: Tween<double>(begin: 0.0, end: 0.0)
-            .chain(CurveTween(curve: Curves.easeInOut)),
-        weight: 92)
-  ]);
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-        vsync: this, duration: Duration(milliseconds: 3000));
-    rotateAnimation = tweenSequence.animate(_controller);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void didUpdateWidget(CollectionCard oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    runAnimation();
-  }
-
-  void runAnimation() {
-    if (widget.collections[widget.index].isEditingBtns) {
-      _controller.repeat(reverse: true);
-    } else {
-      _controller.reset();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        // goToManagerCollections(providerData.id, providerData.title);
-      },
       onLongPress: () {
         BlocProvider.of<CollectionsBloc>(context).add(CollectionsToggleAll());
-        runAnimation();
       },
       child: Padding(
         padding: const EdgeInsets.only(top: 20),
@@ -126,7 +63,7 @@ class _CollectionCardState extends State<CollectionCard>
                             padding: const EdgeInsets.only(
                                 top: 1, right: 1, bottom: 1, left: 1),
                             child: Text(
-                              widget.collections[widget.index].title ?? ' ',
+                              collections[index].title ?? ' ',
                               style: TextStyle(
                                   fontSize: 20.0, color: Colors.black),
                             ),
@@ -144,8 +81,7 @@ class _CollectionCardState extends State<CollectionCard>
                       SizedBox(height: 5.0),
                       FittedBox(
                         child: TextHolder(
-                          titleNameValue:
-                              widget.collections[widget.index].language ?? ' ',
+                          titleNameValue: collections[index].language ?? ' ',
                           fontSize1: 9.0,
                           fontSize2: 15.0,
                         ),
@@ -169,54 +105,51 @@ class _CollectionCardState extends State<CollectionCard>
                 ),
               ),
             ),
-            widget.collections[widget.index].isEditingBtns
+            collections[index].isEditingBtns
                 ? Positioned(
                     top: -1,
                     left: 75,
-                    child: AnimatedBuilder(
-                      animation: rotateAnimation,
-                      builder: (context, child) {
-                        return Transform.rotate(
-                          angle: rotateAnimation.value,
-                          child: child,
-                        );
-                      },
-                      child: Row(
-                        children: <Widget>[
-                          // Edit btn
-                          Btns(
-                              backgroundColor: Colors.white,
-                              icon: Icons.edit,
-                              color: Colors.black54,
-                              onPress: () {
-                                BlocProvider.of<CollectionsBloc>(context)
-                                    .add(CollectionsToggleAll());
-                                _controller.reset();
-                                widget.showEditDialog(
-                                    widget.collections[widget.index]);
-                              }),
-
-                          Btns(
+                    child: Row(
+                      children: <Widget>[
+                        // Edit btn
+                        Btns(
                             backgroundColor: Colors.white,
-                            icon: Icons.delete,
+                            icon: Icons.edit,
                             color: Colors.black54,
                             onPress: () {
-                              deleteConfirmation(context, () {
-                                BlocProvider.of<CollectionsBloc>(context)
-                                  ..add(CollectionsDeleted(
-                                      id: widget.collections[widget.index].id));
-                                Navigator.pop(context);
-                              }, 'Do you want to delete your collection?');
-                            },
-                          ),
-                          SizedBox(width: 5),
-                        ],
-                      ),
-                    ))
-                : Container()
+                              BlocProvider.of<CollectionsBloc>(context)
+                                  .add(CollectionsToggleAll());
+
+                              showEditDialog(collections[index]);
+                            }),
+
+                        Btns(
+                          backgroundColor: Colors.white,
+                          icon: Icons.delete,
+                          color: Colors.black54,
+                          onPress: () {
+                            deleteConfirmation(context, () {
+                              BlocProvider.of<CollectionsBloc>(context)
+                                ..add(CollectionsDeleted(
+                                    id: collections[index].id));
+                              Navigator.pop(context);
+                            }, 'Do you want to delete your collection?');
+                          },
+                        ),
+                        SizedBox(width: 5),
+                      ],
+                    ).shakeAnimation,
+                  )
+                : Container(),
           ],
         ),
       ),
     );
+  }
+}
+
+extension AnimationExtension on Widget {
+  Widget get shakeAnimation {
+    return ShakeAnimation(child: this);
   }
 }
