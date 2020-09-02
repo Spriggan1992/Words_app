8a4b0e969d7890a7eec020f1ee0d0323c5c0fab7
Roman <spriggan1992@gmail.com>
2020-09-02 16:31:33 +0800

    start to change design at training_manager_creen

--- lib/screens/training_manager_screen/training_manager_screen.dart
+++ lib/screens/training_manager_screen/training_manager_screen.dart
@@ -36,125 +36,139 @@ class _TrainingManagerState extends State<TrainingManager> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TrainingBtnsContainers(
-                    defaultSize: defaultSize,
-                    child: ReusableBottomIconBtn(
-                        icons: Icons.fitness_center,
-                        color: Colors.black,
-                        onPress: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Matches(
-                                words: words,
-                              ),
-                            )))),
-                TrainingBtnsContainers(
-                  defaultSize: defaultSize,
-                  child: ReusableBottomIconBtn(
-                    icons: Icons.directions_bike,
-                    color: Colors.black,
-                    onPress: () => Navigator.pushNamed(
-                      context,
-                      PairGame.id,
-                      arguments: {'id': collectionId},
+          // Main container with all content on the page
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(defaultSize * 2),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TitleTextHolder(title: '1. I want to play ...'),
+                  Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TrainingBtnsContainers(
+                            defaultSize: defaultSize,
+                            child: ReusableBottomIconBtn(
+                                icons: Icons.fitness_center,
+                                color: Colors.black,
+                                onPress: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Matches(
+                                        words: words,
+                                      ),
+                                    )))),
+                        TrainingBtnsContainers(
+                          defaultSize: defaultSize,
+                          child: ReusableBottomIconBtn(
+                            icons: Icons.directions_bike,
+                            color: Colors.black,
+                            onPress: () => Navigator.pushNamed(
+                              context,
+                              PairGame.id,
+                              arguments: {'id': collectionId},
+                            ),
+                          ),
+                        ),
+                        TrainingBtnsContainers(
+                          defaultSize: defaultSize,
+                          child: ReusableBottomIconBtn(
+                              icons: Icons.photo_album,
+                              color: Colors.black,
+                              onPress: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Training(
+                                      words: words,
+                                    ),
+                                  ))),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                TrainingBtnsContainers(
-                  defaultSize: defaultSize,
-                  child: ReusableBottomIconBtn(
-                      icons: Icons.photo_album,
-                      color: Colors.black,
-                      onPress: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Training(
-                              words: words,
-                            ),
-                          ))),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            // color: Colors.grey[200],
-            padding: EdgeInsets.symmetric(horizontal: defaultSize * 3),
-            height: defaultSize * 4,
-            width: SizeConfig.blockSizeHorizontal * 100,
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                // Text('Filters'),
-                DropdownButton<String>(
-                  value: dropdownValue,
-                  icon: Icon(Icons.arrow_drop_down),
-                  iconSize: defaultSize * 3,
-                  onChanged: (String newValue) {
-                    setState(() {
-                      dropdownValue = newValue;
-                    });
-                  },
-                  items: <String>[
-                    'Filters',
-                    'Know',
-                    "Don't know",
-                    'Custom list'
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                padding: EdgeInsets.all(defaultSize),
-                margin: EdgeInsets.only(
-                    bottom: defaultSize * 7,
-                    right: defaultSize * 3,
-                    left: defaultSize * 3),
-                child: ListView.builder(
-                    itemCount: words.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        child: ListTile(
-                          leading: Text(words[index].part.partName,
-                              style: TextStyle(
-                                  color: words[index].part.partColor)),
-                          title: Container(
-                              padding: EdgeInsets.only(left: defaultSize * 1),
-                              child: Text(words[index].targetLang ?? '')),
-                          subtitle: Container(
-                              padding: EdgeInsets.only(left: defaultSize * 1),
-                              child: Text(words[index].ownLang ?? '')),
-                        ),
-                      );
-                    }),
+                  TitleTextHolder(title: '2. I want to study words that I ...'),
+                  Container(
+                    child: Row(
+                      children: [
+                        Chip(
+                          label: Text('know'),
+                          shape: Border(),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    // color: Colors.grey[200],
+                    padding: EdgeInsets.symmetric(horizontal: defaultSize * 3),
+                    height: defaultSize * 4,
+                    width: SizeConfig.blockSizeHorizontal * 100,
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        // Text('Filters'),
+                        DropdownButton<String>(
+                          value: dropdownValue,
+                          icon: Icon(Icons.arrow_drop_down),
+                          iconSize: defaultSize * 3,
+                          onChanged: (String newValue) {
+                            setState(() {
+                              dropdownValue = newValue;
+                            });
+                          },
+                          items: <String>[
+                            'Filters',
+                            'Know',
+                            "Don't know",
+                            'Custom list'
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          ReusableMainButton(
-            titleText: 'Go to Trainig',
-            textColor: Colors.black,
-            backgroundColor: Theme.of(context).accentColor,
-            onPressed: () {},
+          //  Button-container
+          Container(
+            child: ReusableMainButton(
+              titleText: 'Go to Trainig',
+              textColor: Colors.black,
+              backgroundColor: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
           ),
         ],
       ),
     );
+  }
+}
+
+class TitleTextHolder extends StatelessWidget {
+  const TitleTextHolder({
+    Key key,
+    @required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultSize = SizeConfig.defaultSize;
+    return Container(
+        child: Text(title,
+            style: TextStyle(
+                fontSize: defaultSize * 2,
+                color: Theme.of(context).accentColor)));
   }
 }
 
@@ -177,9 +191,44 @@ class TrainingBtnsContainers extends StatelessWidget {
         boxShadow: [kBoxShadow],
         color: Colors.white,
       ),
-      width: defaultSize * 12,
-      height: defaultSize * 10,
+      width: defaultSize * 5,
+      height: defaultSize * 5,
       child: child,
     );
   }
 }
+
+// Expanded(
+//                     child: Center(
+//                       child: Container(
+//                         decoration: BoxDecoration(
+//                           color: Colors.white,
+//                         ),
+//                         padding: EdgeInsets.all(defaultSize),
+//                         margin: EdgeInsets.only(
+//                             bottom: defaultSize * 7,
+//                             right: defaultSize * 3,
+//                             left: defaultSize * 3),
+//                         child: ListView.builder(
+//                             itemCount: words.length,
+//                             itemBuilder: (context, index) {
+//                               return Container(
+//                                 child: ListTile(
+//                                   leading: Text(words[index].part.partName,
+//                                       style: TextStyle(
+//                                           color: words[index].part.partColor)),
+//                                   title: Container(
+//                                       padding: EdgeInsets.only(
+//                                           left: defaultSize * 1),
+//                                       child:
+//                                           Text(words[index].targetLang ?? '')),
+//                                   subtitle: Container(
+//                                       padding: EdgeInsets.only(
+//                                           left: defaultSize * 1),
+//                                       child: Text(words[index].ownLang ?? '')),
+//                                 ),
+//                               );
+//                             }),
+//                       ),
+//                     ),
+//                   ),
