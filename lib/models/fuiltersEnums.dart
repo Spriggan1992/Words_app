e851bd41e53ad7dcae822d0e0a53de115b24e6aa
Roman <spriggan1992@gmail.com>
2020-09-04 23:21:46 +0800

    issue with filter words

--- lib/models/fuiltersEnums.dart
+++ lib/models/fuiltersEnums.dart
@@ -1,4 +1,4 @@
 enum Games { bricks, pair, wrongCorrect }
-enum FilterFavorites { all, favorites }
-enum Difficulties { know, knowALittle, dontKnow }
+enum FilterFavorites { favorites, all }
+enum Difficulties { know, knowALittle, dontKnow, none }
 // enum  filterCollections{ know, knowALittle, dontKnow, none }
