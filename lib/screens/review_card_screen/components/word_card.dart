0f49625177094b275ae7aca12e5fd2b72e298692
Roman <spriggan1992@gmail.com>
2020-10-07 13:25:00 +0800

    change PageView.builder in  review_card

--- lib/screens/review_card_screen/components/word_card.dart
+++ lib/screens/review_card_screen/components/word_card.dart
@@ -37,11 +37,11 @@ class WordCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            width: SizeConfig.blockSizeHorizontal * 75,
-            height: SizeConfig.blockSizeVertical * 50,
+            // width: SizeConfig.blockSizeHorizontal * 75,
+            height: SizeConfig.blockSizeVertical * 45,
             decoration: BoxDecoration(
               border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(10.0),
+              borderRadius: BorderRadius.circular(defaultSize * 1),
               color: Colors.white,
               boxShadow: [kBoxShadow],
             ),
@@ -57,20 +57,20 @@ class WordCard extends StatelessWidget {
                       Positioned(
                         top: -1,
                         child: Container(
-                          width: SizeConfig.blockSizeHorizontal * 73,
-                          height: 10,
+                          width: SizeConfig.blockSizeHorizontal * 65.6,
+                          height: defaultSize * 1.2,
                           decoration: BoxDecoration(
                             color: part,
                             borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(defaultSize * 1),
+                              topRight: Radius.circular(defaultSize * 1),
                             ),
                           ),
                         ),
                       ),
                       side == 'front'
                           ? Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
                                 SizedBox(
                                   height: defaultSize * 2,
@@ -83,15 +83,15 @@ class WordCard extends StatelessWidget {
                                 Container(
                                   margin:
                                       EdgeInsets.only(bottom: defaultSize * 1),
-                                  width: defaultSize * 20,
-                                  height: defaultSize * 20,
+                                  width: defaultSize * 19,
+                                  height: defaultSize * 19,
                                   decoration: word.image.path == '' ||
                                           word.image == null ||
                                           word.image.path == null
                                       ? BoxDecoration()
                                       : BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
+                                          borderRadius: BorderRadius.circular(
+                                              defaultSize * 1),
                                           image: DecorationImage(
                                             image: FileImage(word.image),
                                             fit: BoxFit.cover,
@@ -127,12 +127,13 @@ class WordCard extends StatelessWidget {
               ],
             ),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: defaultSize * 2),
           Container(
             height: SizeConfig.blockSizeVertical * 20,
             decoration: innerShadow,
             child: Padding(
-              padding: const EdgeInsets.only(top: 10, left: 10),
+              padding:
+                  EdgeInsets.only(top: defaultSize * 1, left: defaultSize * 1),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
@@ -141,12 +142,12 @@ class WordCard extends StatelessWidget {
                           style: Theme.of(context)
                               .primaryTextTheme
                               .bodyText2
-                              .merge(TextStyle(fontSize: 20.0)))
+                              .merge(TextStyle(fontSize: defaultSize * 2)))
                       : Text(word.exampleTranslations ?? '...',
                           style: Theme.of(context)
                               .primaryTextTheme
                               .bodyText2
-                              .merge(TextStyle(fontSize: 20.0)))
+                              .merge(TextStyle(fontSize: defaultSize * 2)))
                 ],
               ),
             ),
@@ -156,3 +157,161 @@ class WordCard extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:words_app/constants/constants.dart';
+// import 'package:words_app/models/word_model.dart';
+// import 'package:words_app/utils/size_config.dart';
+
+// import 'title_text_holder_container.dart';
+
+// class WordCard extends StatelessWidget {
+//   const WordCard({
+//     Key key,
+//     this.word,
+//     this.index,
+//     this.part,
+//     this.side,
+//     // this.wordHolder,
+//   }) : super(key: key);
+
+//   final int index;
+//   final Color part;
+//   final String side;
+//   final Word word;
+//   // final String targetLang;
+//   // final String secondLang;
+//   // final String thirdLang;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     double defaultSize = SizeConfig.defaultSize;
+
+//     return Container(
+//       padding: EdgeInsets.only(
+//         right: defaultSize * 3,
+//         left: defaultSize * 3,
+//         top: defaultSize * 2,
+//       ),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: <Widget>[
+//           Container(
+//             width: SizeConfig.blockSizeHorizontal * 75,
+//             height: SizeConfig.blockSizeVertical * 44,
+//             decoration: BoxDecoration(
+//               border: Border.all(color: Colors.white),
+//               borderRadius: BorderRadius.circular(10.0),
+//               color: Colors.white,
+//               boxShadow: [kBoxShadow],
+//             ),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Flexible(
+//                   flex: 1,
+//                   child: Stack(
+//                     overflow: Overflow.visible,
+//                     alignment: Alignment.bottomCenter,
+//                     children: <Widget>[
+//                       Positioned(
+//                         top: -1,
+//                         child: Container(
+//                           width: SizeConfig.blockSizeHorizontal * 73,
+//                           height: 10,
+//                           decoration: BoxDecoration(
+//                             color: part,
+//                             borderRadius: BorderRadius.only(
+//                               topLeft: Radius.circular(10),
+//                               topRight: Radius.circular(10),
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                       side == 'front'
+//                           ? Column(
+//                               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                               children: [
+//                                 SizedBox(
+//                                   height: defaultSize * 2,
+//                                 ),
+//                                 TitleTextHolderContainer(
+//                                   defaultSize: defaultSize,
+//                                   wordHolder: word.targetLang ?? '...',
+//                                 ),
+//                                 SizedBox(height: defaultSize * 4),
+//                                 Container(
+//                                   margin:
+//                                       EdgeInsets.only(bottom: defaultSize * 1),
+//                                   width: defaultSize * 20,
+//                                   height: defaultSize * 20,
+//                                   decoration: word.image.path == '' ||
+//                                           word.image == null ||
+//                                           word.image.path == null
+//                                       ? BoxDecoration()
+//                                       : BoxDecoration(
+//                                           borderRadius:
+//                                               BorderRadius.circular(10),
+//                                           image: DecorationImage(
+//                                             image: FileImage(word.image),
+//                                             fit: BoxFit.cover,
+//                                           ),
+//                                         ),
+//                                 )
+//                               ],
+//                             )
+//                           : Container(
+//                               alignment: Alignment.center,
+//                               child: Column(
+//                                 mainAxisAlignment:
+//                                     MainAxisAlignment.spaceAround,
+//                                 children: [
+//                                   TitleTextHolderContainer(
+//                                     defaultSize: defaultSize,
+//                                     wordHolder: word.ownLang ?? '...',
+//                                   ),
+//                                   TitleTextHolderContainer(
+//                                     defaultSize: defaultSize,
+//                                     wordHolder: word.secondLang ?? '...',
+//                                   ),
+//                                   TitleTextHolderContainer(
+//                                     defaultSize: defaultSize,
+//                                     wordHolder: word.thirdLang ?? '...',
+//                                   )
+//                                 ],
+//                               ),
+//                             )
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           SizedBox(height: 20),
+//           Container(
+//             height: SizeConfig.blockSizeVertical * 20,
+//             decoration: innerShadow,
+//             child: Padding(
+//               padding: const EdgeInsets.only(top: 10, left: 10),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: <Widget>[
+//                   side == 'front'
+//                       ? Text(word.example ?? '...',
+//                           style: Theme.of(context)
+//                               .primaryTextTheme
+//                               .bodyText2
+//                               .merge(TextStyle(fontSize: 20.0)))
+//                       : Text(word.exampleTranslations ?? '...',
+//                           style: Theme.of(context)
+//                               .primaryTextTheme
+//                               .bodyText2
+//                               .merge(TextStyle(fontSize: 20.0)))
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/screens/review_card_screen/review_card.dart
+++ lib/screens/review_card_screen/review_card.dart
@@ -48,7 +48,7 @@ class _ReviewCardState extends State<ReviewCard>
     super.initState();
     getCurrInd();
     _pageController =
-        PageController(viewportFraction: 0.87, initialPage: initialPage);
+        PageController(viewportFraction: 0.8, initialPage: initialPage);
 
     page = widget.index;
   }
@@ -68,7 +68,7 @@ class _ReviewCardState extends State<ReviewCard>
   /// Pass throught [initialPage] what we get from [screens/manager_collection/components/word_card.dart]
   void getCurrInd() {
     initialPage = widget.index;
-    Timer(Duration(milliseconds: 100), () {
+    Timer(Duration(milliseconds: 500), () {
       setState(() {});
     });
   }
@@ -101,52 +101,36 @@ class _ReviewCardState extends State<ReviewCard>
                   );
                 },
                 controller: _pageController,
-                physics: ClampingScrollPhysics(),
                 itemCount: widget.words.length,
                 itemBuilder: (context, index) {
-                  return AnimatedBuilder(
-                    animation: _pageController,
-                    builder: (context, child) {
-                      double value = 0;
-                      if (_pageController.position.haveDimensions) {
-                        value = index - _pageController.page;
-                        value = (value * 0.06).clamp(-1, 1);
-                      }
+                  bool active = index == page;
+                  final double top = active ? defaultSize * 1 : defaultSize * 4;
+                  return AnimatedContainer(
+                    duration: Duration(milliseconds: 1000),
+                    // curve: Curves.easeInOut,
+                    curve: Curves.easeOutQuint,
+                    margin: EdgeInsets.only(top: top, bottom: 10),
 
-                      return AnimatedOpacity(
-                        duration: Duration(milliseconds: 500),
-                        opacity: initialPage == index ? 1 : 0.1,
-                        child: Transform.rotate(
-                          angle: math.pi * value,
-                          child: Stack(
-                            fit: StackFit.expand,
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              FlipCard(
-                                onFlip: () {
-                                  setState(() {
-                                    toggleIsFront();
-                                  });
-                                },
-                                direction: FlipDirection.HORIZONTAL,
-                                speed: 400,
-                                front: WordCard(
-                                  word: widget.words[index],
-                                  side: 'front',
-                                  index: index,
-                                  part: widget.words[index].part.partColor,
-                                ),
-                                back: WordCard(
-                                  word: widget.words[index],
-                                  index: index,
-                                  part: widget.words[index].part.partColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
+                    child: FlipCard(
+                      onFlip: () {
+                        setState(() {
+                          toggleIsFront();
+                        });
+                      },
+                      direction: FlipDirection.HORIZONTAL,
+                      speed: 400,
+                      front: WordCard(
+                        word: widget.words[index],
+                        side: 'front',
+                        index: index,
+                        part: widget.words[index].part.partColor,
+                      ),
+                      back: WordCard(
+                        word: widget.words[index],
+                        index: index,
+                        part: widget.words[index].part.partColor,
+                      ),
+                    ),
                   );
                 },
               ),
@@ -155,7 +139,7 @@ class _ReviewCardState extends State<ReviewCard>
           Container(
             margin: EdgeInsets.only(bottom: defaultSize * 2),
             height: defaultSize * 5,
-            width: SizeConfig.blockSizeHorizontal * 75,
+            width: SizeConfig.blockSizeHorizontal * 78,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -211,3 +195,217 @@ class _ReviewCardState extends State<ReviewCard>
     );
   }
 }
+// import 'dart:async';
+// import 'dart:math' as math;
+
+// import 'package:flip_card/flip_card.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:words_app/bloc/words/words_bloc.dart';
+
+// import 'package:words_app/widgets/base_appbar.dart';
+// import 'package:words_app/constants/constants.dart';
+
+// import 'package:words_app/models/difficulty.dart';
+
+// import '../../bloc/words/words_bloc.dart';
+// import '../../widgets/base_appbar.dart';
+// import '../../models/difficulty.dart';
+// import '../../models/word_model.dart';
+// import '../../utils/size_config.dart';
+// import 'components/word_card.dart';
+
+// class ReviewCard extends StatefulWidget {
+//   static String id = 'review_card_screen';
+//   ReviewCard({this.index, this.words});
+//   final int index;
+//   final List<Word> words;
+
+//   @override
+//   _ReviewCardState createState() => _ReviewCardState();
+// }
+
+// class _ReviewCardState extends State<ReviewCard>
+//     with SingleTickerProviderStateMixin {
+//   PageController _pageController;
+
+//   /// Initial index of page;
+//   int initialPage;
+
+//   /// If `true` show front - examples text
+//   bool isFront = true;
+//   String selectedChoice = "";
+
+//   /// Initial page from index
+//   int page;
+//   List<Difficulty> difficultyList = DifficultyList().difficultyList;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     getCurrInd();
+//     _pageController =
+//         PageController(viewportFraction: 0.87, initialPage: initialPage);
+
+//     page = widget.index;
+//   }
+
+//   @override
+//   void dispose() {
+//     _pageController.dispose();
+//     super.dispose();
+//   }
+
+//   /// Set up [isFront] `true` or `false`
+//   void toggleIsFront() {
+//     isFront = !isFront;
+//     setState(() {});
+//   }
+
+//   /// Pass throught [initialPage] what we get from [screens/manager_collection/components/word_card.dart]
+//   void getCurrInd() {
+//     initialPage = widget.index;
+//     Timer(Duration(milliseconds: 500), () {
+//       setState(() {});
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     SizeConfig().init(context);
+//     double defaultSize = SizeConfig.defaultSize;
+
+//     return Scaffold(
+//       appBar: BaseAppBar(
+//         title: Text(
+//           "Collection's name",
+//         ),
+//         actions: <Widget>[],
+//         appBar: AppBar(),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             child: Container(
+//               child: PageView.builder(
+//                 onPageChanged: (value) {
+//                   setState(
+//                     () {
+//                       initialPage = value;
+//                       page = _pageController.page.round();
+//                       selectedChoice = '';
+//                     },
+//                   );
+//                 },
+//                 controller: _pageController,
+//                 physics: ClampingScrollPhysics(),
+//                 itemCount: widget.words.length,
+//                 itemBuilder: (context, index) {
+//                   return AnimatedBuilder(
+//                     animation: _pageController,
+//                     builder: (context, child) {
+//                       double value = 0;
+//                       if (_pageController.position.haveDimensions) {
+//                         value = index - _pageController.page;
+//                         value = (value * 0.04).clamp(-1, 1);
+//                       }
+
+//                       return AnimatedOpacity(
+//                         duration: Duration(milliseconds: 600),
+//                         // opacity: initialPage == index ? 1 : 0.5,
+//                         opacity: 1,
+//                         child: Transform.rotate(
+//                           angle: math.pi * value,
+//                           child: Stack(
+//                             fit: StackFit.expand,
+//                             alignment: Alignment.center,
+//                             children: <Widget>[
+//                               FlipCard(
+//                                 onFlip: () {
+//                                   setState(() {
+//                                     toggleIsFront();
+//                                   });
+//                                 },
+//                                 direction: FlipDirection.HORIZONTAL,
+//                                 speed: 400,
+//                                 front: WordCard(
+//                                   word: widget.words[index],
+//                                   side: 'front',
+//                                   index: index,
+//                                   part: widget.words[index].part.partColor,
+//                                 ),
+//                                 back: WordCard(
+//                                   word: widget.words[index],
+//                                   index: index,
+//                                   part: widget.words[index].part.partColor,
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       );
+//                     },
+//                   );
+//                 },
+//               ),
+//             ),
+//           ),
+//           Container(
+//             margin: EdgeInsets.only(bottom: defaultSize * 2),
+//             height: defaultSize * 5,
+//             width: SizeConfig.blockSizeHorizontal * 78,
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: [
+//                 ...List.generate(
+//                   difficultyList.length,
+//                   (index) => Container(
+//                     padding: const EdgeInsets.all(5.0),
+//                     child: ChoiceChip(
+//                       labelPadding:
+//                           EdgeInsets.symmetric(horizontal: defaultSize * 0.7),
+//                       elevation: 5,
+//                       padding: EdgeInsets.symmetric(
+//                         horizontal: defaultSize * 0.6,
+//                         vertical: defaultSize,
+//                       ),
+//                       label: Text(difficultyList[index].name),
+//                       labelStyle:
+//                           Theme.of(context).primaryTextTheme.bodyText2.merge(
+//                                 TextStyle(
+//                                   color: Colors.black,
+//                                   fontSize: 12.0,
+//                                   fontWeight: FontWeight.bold,
+//                                 ),
+//                               ),
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(5.0),
+//                       ),
+//                       backgroundColor: difficultyList[index].color,
+//                       selected: selectedChoice == difficultyList[index].name,
+//                       onSelected: (selected) {
+//                         setState(
+//                           () {
+//                             selectedChoice = difficultyList[index].name;
+//                           },
+//                         );
+//                         context.bloc<WordsBloc>().add(
+//                               WordsUpdatedWord(
+//                                 word: widget.words[page].copyWith(
+//                                     difficulty:
+//                                         difficultyList[index].difficulty),
+//                               ),
+//                             );
+//                         context.bloc<WordsBloc>().add(WordsLoaded());
+//                       },
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
